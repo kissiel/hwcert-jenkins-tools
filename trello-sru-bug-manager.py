c0489015d39a57f841d11f7c7c50ae6577875f23
Paul Larson <paul.larson@canonical.com>
2018-04-03 22:59:43 -0500

    Use exceptions rather than returning None

--- trello-sru-bug-manager.py
+++ trello-sru-bug-manager.py
@@ -18,7 +18,6 @@
 #        Paul Larson <paul.larson@canonical.com>
 
 import argparse
-import logging
 import os
 import re
 import sys
@@ -39,7 +38,7 @@ class LPHelper:
             bug = self.project.searchTasks(search_text=search_text)[0]
             return SruBug(bug)
         except (TypeError, IndexError):
-            return None
+            raise LookupError
 
 
 class TrelloHelper:
@@ -53,15 +52,14 @@ class TrelloHelper:
             lane_name = l.name.lower()
             if lane_name == lane_id:
                 return l
-        return None
+        raise LookupError('Trello lane "{}" not found!'.format(lane_id))
 
     def search_cards_in_lane(self, lane_id, search_text):
         """Yield cards with search_text in lane_id one at a time"""
         lane = self._get_lane(lane_id)
-        if lane:
-            for card in lane.list_cards():
-                if search_text in card.name:
-                    yield card
+        for card in lane.list_cards():
+            if search_text in card.name:
+                yield card
 
 
 class SruBug:
@@ -76,7 +74,7 @@ class SruBug:
         for task in self.bug.bug.bug_tasks:
             if task_name in str(task):
                 return task
-        return None
+        raise LookupError
 
     def set_task_state(self, task_name, state):
         for task in self.bug.bug.bug_tasks:
@@ -109,7 +107,7 @@ def get_card_snap_version(card):
         r"\((?P<revision>.*?)\)(?:\s+\-\s+\[(?P<track>.*?)\])?", card.name)
     if m:
         return m.group('version')
-    return None
+    raise ValueError
 
 
 def get_args():
@@ -142,17 +140,26 @@ def main():
     for card in trello.search_cards_in_lane('beta', '-kernel'):
         # If we can't get version from title, it's not formatted how we
         # expect, so ignore it
-        version = get_card_snap_version(card)
-        if not version:
+        try:
+            version = get_card_snap_version(card)
+        except ValueError:
             continue
 
         if not card_ready_for_candidate(card):
             continue
 
-        bug = lp.find_bug(version)
+        try:
+            bug = lp.find_bug(version)
+        except LookupError:
+            print('No bug found for {}!'.format(version))
+            continue
         # If the bug is already fix-released, there's nothing more to do
         TARGET_TASK = 'snap-certification-testing'
-        if bug.get_task_state(TARGET_TASK).status == 'Fix Released':
+        try:
+            if bug.get_task_state(TARGET_TASK).status == 'Fix Released':
+                continue
+        except LookupError:
+            print('No task called "{}" found!'.format(TARGET_TASK))
             continue
 
         # This is the bug for the card we found, mark the task complete and
