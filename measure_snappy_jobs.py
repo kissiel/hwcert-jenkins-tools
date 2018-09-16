4f4242ec8907fe0d6bb5f994ce462c29075c4862
Maciej Kisielewski <maciej.kisielewski@canonical.com>
2018-09-16 21:10:09 +0200

    add script pulling snappy-jobs duration from submission.json
    Signed-off-by: Maciej Kisielewski <maciej.kisielewski@canonical.com>

---
+++ measure_snappy_jobs.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# Copyright (C) 2018 Canonical Ltd
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Written by:
+#       Maciej Kisielewski <maciej.kisielewski@canonical.com>
+
+import json
+import sys
+import time
+
+MEASURED_JOBS = [
+        'snap-install',
+        'snap-remove',
+]
+
+def dquote(s):
+    # surround s with double quotes
+    return '"{}"'.format(s)
+
+class InfluxQueryWriter():
+    TMPL = ("INSERT snap_timing,project_name={proj},job_name={job},"
+            "hw_id={hw},os_kind={os} elapsed={elapsed} {tstamp}")
+
+    def __init__(self, submission):
+        self._proj = dquote(submission.get('title', 'unknown'))
+        # XXX: In theory the explicit timestamp is not needed, as influx would
+        #      use time of insert as time of measurement, but since there are
+        #      multiple measurements from one submission, let's use the sime
+        #      timestamp
+        self._time = int(time.time() * 10 ** 9)  # timestamp in nanoseconds
+        # TODO: figure out how to get hardware info
+        self._hw_id = dquote('unknown')
+        self._os_kind = dquote(submission.get('distribution', dict()).get(
+            'description', 'unknown'))
+        self._results = submission.get('results', [])
+
+    def get_influx_inserts(self):
+        for result in self._results:
+            for job in MEASURED_JOBS:
+                if result['id'].endswith(job):
+                    if not result.get('duration'):
+                        continue
+                    yield InfluxQueryWriter.TMPL.format(
+                        proj=self._proj,
+                        job=dquote(job),
+                        hw=self._hw_id,
+                        os=self._os_kind,
+                        elapsed=result['duration'],
+                        tstamp=self._time)
+
+
+
+
+def main():
+    if len(sys.argv) < 2:
+        raise SystemExit('Usage: {} submission.json'.format(sys.argv[0]))
+    try:
+        with open(sys.argv[1], 'rt') as f:
+            content = json.load(f)
+            iqw = InfluxQueryWriter(content)
+            for insert in iqw.get_influx_inserts():
+                print(insert)
+    except Exception as exc:
+        raise exc
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ test_measure_snappy_jobs.py
@@ -0,0 +1,71 @@
+# Copyright (C) 2018 Canonical Ltd
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Written by:
+#       Maciej Kisielewski <maciej.kisielewski@canonical.com>
+
+import unittest
+
+from unittest.mock import MagicMock
+
+from measure_snappy_jobs import InfluxQueryWriter
+
+
+class InfluxQueryWriterTests(unittest.TestCase):
+    def test_no_results_no_prints(self):
+        submission = {
+            'results': [],
+        }
+        iqw = InfluxQueryWriter(submission)
+        self.assertEqual(list(iqw.get_influx_inserts()), [])
+
+    def test_one_result_no_meta_infos(self):
+        submission = {
+            'results': [{
+                'id': 'snap-install',
+                'duration': 0.5,
+            }],
+        }
+        with unittest.mock.patch('time.time', MagicMock(return_value=1)):
+            iqw = InfluxQueryWriter(submission)
+            expected = ('INSERT snap_timing,project_name="unknown",'
+                        'job_name="snap-install",hw_id="unknown",'
+                        'os_kind="unknown" elapsed=0.5 1000000000')
+            self.assertEqual(list(iqw.get_influx_inserts()), [expected])
+
+    def test_empty_suspension(self):
+        iqw = InfluxQueryWriter(dict())
+        self.assertEqual(list(iqw.get_influx_inserts()), [])
+
+    def test_full_meta(self):
+        submission = {
+            'distribution': {'description': 'Ubuntu'},
+            'duration': 0.5,
+            'id': 'snap-install',
+            'title': 'checkbox-project',
+            'results': [
+                {'id': 'snap-install', 'duration': 1.5},
+                {'id': 'snap-remove', 'duration': 2.5},
+            ],
+        }
+        expected1 = ('INSERT snap_timing,project_name="checkbox-project",'
+                     'job_name="snap-install",hw_id="unknown",'
+                     'os_kind="Ubuntu" elapsed=1.5 1000000000')
+        expected2 = ('INSERT snap_timing,project_name="checkbox-project",'
+                     'job_name="snap-remove",hw_id="unknown",'
+                     'os_kind="Ubuntu" elapsed=2.5 1000000000')
+        with unittest.mock.patch('time.time', MagicMock(return_value=1)):
+            iqw = InfluxQueryWriter(submission)
+            self.assertEqual(
+                list(iqw.get_influx_inserts()), [expected1, expected2])
