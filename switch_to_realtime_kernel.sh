f36bf9d2c0b7bc9f11ebb4ce6b8f668432f5b937
Maciej Kisielewski <maciej.kisielewski@canonical.com>
2023-07-26 22:49:51 +0200

    wip3

--- switch_to_realtime_kernel.sh
+++ switch_to_realtime_kernel.sh
@@ -3,6 +3,6 @@
 #export GRUB_ENTRY=$(grep menuentry /boot/grub/grub.cfg  | awk -F\' '{print $2}' |grep $TARGET_KERNEL$ | tail -n1)
 export GRUB_MENU_ENTRY=$(sudo grub-mkconfig | grep -iE "menuentry 'Ubuntu, with Linux" | awk '{print i++ " : "$1, $2, $3, $4, $5, $6, $7}' |grep realtime\' | cut -f 1 -d ' ' )
 echo $GRUB_ENTRY
-sudo sed -i 's/^GRUB_DEFAULT=.*$/GRUB_DEFAULT="'1>${GRUB_MENU_ENTRY}'"/' /etc/default/grub
+sudo sed -i 's/^GRUB_DEFAULT=.*$/GRUB_DEFAULT="1>${GRUB_MENU_ENTRY}"/' /etc/default/grub
 sudo update-grub
 ~
